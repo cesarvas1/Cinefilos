bae7437e9a230baeeda8fba701dd2be6096e1f41
CESAR JOSE VASQUEZ GONZALEZ <ISNP024924@ugb.edu.sv>
2024-11-03 16:47:07 -0600

    Clase

--- Models/peliculas.cs
+++ Models/peliculas.cs
@@ -1,6 +1,19 @@
-﻿namespace Cinefilos.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Cinefilos.Models
 {
-    public class peliculas
+    public class PeliculasCinefilos
     {
+        [Key]
+        public int idpeliculas { get; set; }
+        public string Titulo { get; set; }
+        public string Director { get; set; }
+        public string Sinopsis { get; set; }
+        public int Duracion { get; set; }
+        public string Clasificacion { get; set; }
+        public string Genero { get; set; }
+
     }
 }
+
+
